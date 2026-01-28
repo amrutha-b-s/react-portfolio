412e507e9f00992ac1fc3e994131f5b07e697091
amrutha-b-s <1da22is069.is@drait.edu.in>
2026-01-28 19:46:10 +0530

    Initial React portfolio

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import Navbar from "./components/Navbar";
+import About from "./components/About";
+import Skills from "./components/Skills";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+
+      <h1>Hello Amrutha 👋</h1>
+      <p>My first React app is running</p>
+
+      <About />
+      <Skills />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/About.jsx
@@ -0,0 +1,12 @@
+function About() {
+  return (
+    <section>
+      <h2>About Me</h2>
+      <p>
+        I'm learning React step by step and building my first portfolio project.
+      </p>
+    </section>
+  );
+}
+
+export default About;
